4e26f201021c18c7e8f8ea221f2ae70059ef2bad
zhaozilong <1162582191@qq.com>
2020-03-01 18:36:23 +0800

    2020年3月1日18:36:09 修改代码

--- myesdemo/src/main/java/com/zb/EsApp.java
+++ myesdemo/src/main/java/com/zb/EsApp.java
@@ -11,13 +11,7 @@ import java.util.List;
 @SpringBootApplication
 public class EsApp {
     public static void main(String[] args) throws Exception {
-         //写完代码了
-	  //李四提交到李四的分支上，并合并到master
-
-	    //张三也写完了，后写完
-
-	 //李四提交了，先写完
-
+        //2020年3月1日18:34:48 修改代码
         ConfigurableApplicationContext run = SpringApplication.run(EsApp.class, args);
         CourseService bean = run.getBean(CourseService.class);
         List<Course> curseByPage = bean.findCurseByPage(1, 2, "spring", null, null, null);
