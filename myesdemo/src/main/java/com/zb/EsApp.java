741c29f0c5c26701ba11b76c26550e939c7f35e3
w189 <57605950+w189@users.noreply.github.com>
2020-03-01 18:01:38 +0800

    Update EsApp.java

--- myesdemo/src/main/java/com/zb/EsApp.java
+++ myesdemo/src/main/java/com/zb/EsApp.java
@@ -11,8 +11,8 @@ import java.util.List;
 @SpringBootApplication
 public class EsApp {
     public static void main(String[] args) throws Exception {
-
          //写完代码了
+	  //李四提交到李四的分支上，并合并到master
 	
         ConfigurableApplicationContext run = SpringApplication.run(EsApp.class, args);
         CourseService bean = run.getBean(CourseService.class);
