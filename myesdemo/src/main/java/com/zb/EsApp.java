495a581f75bc1ee72702dbef513ea1a82135b5de
w189 <57605950+w189@users.noreply.github.com>
2020-03-01 18:04:00 +0800

    Update EsApp.java

--- myesdemo/src/main/java/com/zb/EsApp.java
+++ myesdemo/src/main/java/com/zb/EsApp.java
@@ -13,7 +13,7 @@ public class EsApp {
     public static void main(String[] args) throws Exception {
          //写完代码了
 	  //李四提交到李四的分支上，并合并到master
-	
+	 //李四提交了，先写完
         ConfigurableApplicationContext run = SpringApplication.run(EsApp.class, args);
         CourseService bean = run.getBean(CourseService.class);
         List<Course> curseByPage = bean.findCurseByPage(1, 2, "spring", null, null, null);
